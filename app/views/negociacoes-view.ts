f9f6bbc2cfaae4796b3a660e1d13016f53505a41
Adriano Santos <adriano_angelo@live.com>
2023-07-24 14:30:46 -0300

    Criando nossa primeira view

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,18 @@
+export class NegociacoesView {
+
+    tamplate(): string {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        `
+    }
+}
