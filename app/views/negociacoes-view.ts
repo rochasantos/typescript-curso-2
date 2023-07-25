23e6f3f113bac5436106bc4ab9dc726bcc989cb0
Adriano Santos <adriano_angelo@live.com>
2023-07-25 19:38:21 -0300

    Métodos privados

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -18,7 +18,7 @@ export class NegociacoesView extends View<Negociacoes> {
                     ${model.lista().map((negociacao: Negociacao) => {
                        return `
                             <tr>
-                                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td>${this.formatarData(negociacao.data)}</td>
                                 <td>${negociacao.quantidade}</td>
                                 <td>${negociacao.valor}</td>
                             </tr>
@@ -27,5 +27,9 @@ export class NegociacoesView extends View<Negociacoes> {
                 </tbody>
             </table>
         `
+    }
+
+    private formatarData(data: Date): string {
+        return new Intl.DateTimeFormat().format(data)
     }
 }
