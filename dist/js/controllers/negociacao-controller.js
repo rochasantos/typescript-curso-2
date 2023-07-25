05227f806431b49f1dc42525341bb25088fbe791
Adriano Santos <adriano_angelo@live.com>
2023-07-25 19:55:34 -0300

    Aceitando apenas dias úteis

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -15,9 +15,14 @@ export class NegociacaoController {
     adiciona() {
         const negociacao = this.criaNegociacao();
         negociacao.data.setDate(12);
-        this.negociacoes.adiciona(negociacao);
-        this.atualizaView();
-        this.limparFormulario();
+        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
+            this.negociacoes.adiciona(negociacao);
+            this.limparFormulario();
+            this.atualizaView();
+        }
+        else {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas.');
+        }
     }
     criaNegociacao() {
         const exp = /-/g;
