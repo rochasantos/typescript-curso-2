fc5f1378593b89d331d033c9a1e18ba6c6ca7ae7
Adriano Santos <adriano_angelo@live.com>
2023-07-25 20:49:33 -0300

    Métodos estáticos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -13,4 +13,12 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
+
+    public static criaDe(dataStr: string, quantidadeStr: string, valorStr: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataStr.replace(exp, ','));
+        const quantidade = parseInt(quantidadeStr);
+        const valor = parseFloat(valorStr);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -11,4 +11,11 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
+    static criaDe(dataStr, quantidadeStr, valorStr) {
+        const exp = /-/g;
+        const date = new Date(dataStr.replace(exp, ','));
+        const quantidade = parseInt(quantidadeStr);
+        const valor = parseFloat(valorStr);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
