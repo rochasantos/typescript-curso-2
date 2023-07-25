768a23c4e422544daeef4c65d6a1fac9f6b4cefe
Adriano Santos <adriano_angelo@live.com>
2023-07-25 17:38:04 -0300

    Herança e pequena surpresa

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,11 +1,7 @@
-export class MensagemView {
+import { View } from "./view.js";
 
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
-
+export class MensagemView extends View{
+    
     template(model: string): string {
         return `
             <p class="alert alert-info">${model}</p>
