71b29d50ed0a8d9333e076d7164214e0e1e873e0
Adriano Santos <adriano_angelo@live.com>
2023-07-26 09:42:47 -0300

    Ultimos ajustes: cacheando e enviando mensagem de erro para o dev

--- app/views/view.ts
+++ app/views/view.ts
@@ -3,8 +3,13 @@ export abstract class View<T> {
     protected elemento: HTMLElement;
     private escapar = false;
     
-    constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor);
+    constructor(seletor: string, escapar?: boolean) {        
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM.`)
+        }
         if (escapar) {
             this.escapar = escapar
         }
